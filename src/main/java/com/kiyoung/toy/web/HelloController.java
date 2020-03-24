aa54bec2240e2c5612f11a998381fd7cadb284d3
liki <>
2020-03-24 22:25:13 +0900

    toy-001 controller & test 작성(2) - 롬복 적용 - requestparam 실험

--- src/main/java/com/kiyoung/toy/web/HelloController.java
+++ src/main/java/com/kiyoung/toy/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.kiyoung.toy.web;
 
+import com.kiyoung.toy.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,6 +12,12 @@ public class HelloController
     @GetMapping("/hello")
     public String hello(){
         return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount)
+    {
+        return new HelloResponseDto(name, amount);
     }
 
 }
